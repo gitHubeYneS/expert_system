2187ebf84fa923d75668a2f0aa11e5460a6979fe
gitHubeYneS <karlyuk95@gmail.com>
2018-06-27 15:10:18 +0300

    separated modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpackConfigs/pug');
+const devserver = require('./webpackConfigs/devserver');
 
-const PATHS={
-    src:path.join(__dirname,'src'),
-    build:path.join(__dirname,'build')
+
+
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
 }
 
-module.exports={
-    entry:PATHS.src+'/app.js',
-    output:{
-        path:PATHS.build,
-        filename:'[name].bundle.js'
+//общий код для разработки и продакшена
+const common = merge([
+    {
+    entry: PATHS.src + '/app.js',
+    output: {
+        path: PATHS.build,
+        filename: '[name].bundle.js'
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            title:'Expert system',
-            template:PATHS.src+'/index.pug',
-            hash:true
+            title: 'Expert system',
+            template: PATHS.src + '/index.pug',
+            hash: true
         })
-    ],
-    module:{
-        rules:[{
-            test:/\.pug$/,
-            loader:'pug-loader',
-            options:{
-                pretty:true
-            }
-        }]
+    ]
+},
+    pug()
+]);
+
+module.exports = function (env) {
+    if (env === 'production') {
+        return common;
+    }
+    if (env === 'development') {
+        return merge([{}, common, devserver()])
     }
 };
